9b3450c08dfdbb87317d3028ae5a4f970b598797
MaYoT <majot15@outlook.com>
2022-08-29 19:56:24 +0200

    Wyczyszczenie projektu z utworzonych przed .NET przykładowych plików i stworzenie podstawowego kontrolera wraz z GETem

---
+++ Repositories/InMemItemsRepository.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using WSEIbackendREST.Entities;
+using System.Linq;
+
+namespace WSEIbackendREST.Repositories
+{
+    public class InMemItemsRepository
+    {
+        private readonly List<Item> items = new List<Item>()
+        {
+            new Item { Id = Guid.NewGuid(), Name = "Combustion Engine", Price = 250000, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Gearbox", Price = 150000, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Electric Engine", Price = 120000, CreatedDate = DateTimeOffset.UtcNow }
+        };
+
+        public IEnumerable<Item> GetItems()
+        {
+            return items;
+        }
+
+        public Item GetItem(Guid id)
+        {
+            return items.Where(item => item.Id == id).SingleOrDefault();
+        }
+    }
+}
